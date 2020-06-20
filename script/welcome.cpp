c267b3227473c049a8919cb16bb6b0cabf8ddbdd
Agnibh <deyagnibh@gmail.com>
2020-06-20 16:09:31 +0000

    deleted build, modified C++ script

--- script/welcome.cpp
+++ script/welcome.cpp
@@ -6,7 +6,7 @@ namespace gazebo
   {
     public: WorldPluginMyRobot() : WorldPlugin()
             {
-              printf("Welcome to World!\n");
+              printf("Welcome to Agnibh's World!\n");
             }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
